93cc2cc573cbb451ad050e6612b3cc01362ae71c
RyanBDev <ryanbock4.0@gmail.com>
2023-11-05 13:27:24 +1000

    Removed handlebars, code cleanup.

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 const cors = require('cors');
+const helmet = require('helmet');
 
 require('dotenv').config();
 
-var indexRouter = require('./routes/index');
+const indexRouter = require('./routes/index');
 
-var app = express();
+const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
+app.use(helmet());
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
@@ -31,13 +30,13 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
+  // If the page is not found (i.e. no route) output this, else just display the err message
   res.status(err.status || 500);
-  res.render('error');
+  if (res.statusCode == 404) {
+    res.json({ status: "Error", message: "Page not found!" });
+  } else {
+    res.json({ status: "Error", message: err.message});
+  }
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,28 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const AWS = require('aws-sdk');
 
 AWS.config.update({
-    region: "ap-southeast-2",
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  sessionToken: process.env.AWS_SESSION_TOKEN,
+  region: "ap-southeast-2",
 });
 
 // Initialise s3 variables
-const bucketName = 'n11069449-compress-store';
 const s3 = new AWS.S3({ apiVersion: "2006-03-01" });
+const bucketName = 'n11069449-compress-store';
+
 
 // Initialise sqs variables
 const sqs = new AWS.SQS();
+const queueName = 'n11069449-sqs-queue';
 
 router.get('/upload',(req, res) => {
   const fileName = req.query.fileName;
   const fileType = req.query.fileType;
 
+  // Create bucket if it does not exist, do nothing if it does.
   s3.createBucket({ Bucket: bucketName })
   .promise()
   .then(() => console.log(`Created bucket: ${bucketName}`))
@@ -27,6 +33,7 @@ router.get('/upload',(req, res) => {
      }
   });
   
+  // Setup params to generate presigned url that expires in 60 seconds
   const s3Params = {
     Bucket: bucketName,
     Key: fileName,
@@ -37,25 +44,25 @@ router.get('/upload',(req, res) => {
   s3.getSignedUrl('putObject', s3Params, (err, data) => {
     if (err) {
       console.log(err);
-      return res.end();
+      res.status(500);
+      res.json({ status: "Error", message: "Could not generate the pre-signed S3 url." });
+    } else {
+      res.json({
+        signedRequest: data,
+        url: `https://${s3Params.Bucket}.s3.amazonaws.com/${fileName}`
+      });
     }
-    res.json({
-      signedRequest: data,
-      url: `https://${s3Params.Bucket}.s3.amazonaws.com/${fileName}`
-    });
   }); 
 });
 
 router.post('/enqueue', async (req, res) => {
-  const queueName = 'n11069449-sqs-queue';
-
   // Create the queue and wait for the queue to be created.
   try {
     const createQueueData = await sqs.createQueue({
       QueueName: queueName,
       Attributes: {
-        DelaySeconds: '60',
-        MessageRetentionPeriod: '86400'
+        DelaySeconds: '60',   // Messages in the queue won't be visible to consumers for 60 seconds
+        MessageRetentionPeriod: '86400'   // Retain unprocessed messages for 1 day (24 * 60 * 60 seconds)
       }
     }).promise();
 
@@ -63,16 +70,18 @@ router.post('/enqueue', async (req, res) => {
 
     const sendMessageData = await sqs.sendMessage({
       MessageBody: JSON.stringify({ 
-        fileKeys: req.body.fileKeys,
-        folderKey: req.body.folderKey
-      }),
+          fileKeys: req.body.fileKeys,
+          folderKey: req.body.folderKey
+        }),
       QueueUrl: queueUrl
     }).promise();
 
-    res.status(200).send(sendMessageData);
+    res.status(200);
+    res.json(sendMessageData);
   } catch (err) {
     console.log('Error', err);
-    res.status(500).send(err);
+    res.status(500)
+    res.json({ status: "Error", message: "Failed to queue the file compression." });
   }
 });
 
